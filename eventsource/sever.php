b6ccd42d139c1b208c85e580dc3bc1231754d824
uniqueWTS <710029721@qq.com>
2018-11-03 21:35:58 +0800

    fix MIME type

--- eventsource/sever.php
+++ eventsource/sever.php
@@ -1,7 +1,9 @@
 <?php 
-header('Content-Type: text/event-stream'); 
+# header('Content-Type: text/event-stream');
+header('Content-Type: application/x-httpd-php');  
 header('Cache-Control: no-cache'); 
-
+# error: response has a MIME type : application/x-httpd-php
+# expect text/event-stream
 $time = date('r'); 
 echo "data: The server time is: {$time}\n\n"; 
 flush(); 
